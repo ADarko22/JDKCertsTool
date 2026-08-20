de70b6341fe5051e1b2fbbf9afc531bde0554e1c
ADarko22 <a.elessar22@gmail.com>
2026-08-20 19:46:03 +0200

    chore: fix code linting

--- src/test/kotlin/edu/adarko22/jdkcerts/infra/system/unix/UNIXJdkPathsDiscoveryTest.kt
+++ src/test/kotlin/edu/adarko22/jdkcerts/infra/system/unix/UNIXJdkPathsDiscoveryTest.kt
@@ -1,9 +1,9 @@
 package edu.adarko22.jdkcerts.infra.system.unix
 
+import edu.adarko22.jdkcerts.core.system.SystemInfoProvider
 import edu.adarko22.jdkcerts.createDir
 import edu.adarko22.jdkcerts.createExecutableFile
 import edu.adarko22.jdkcerts.createValidJdkPath
-import edu.adarko22.jdkcerts.core.system.SystemInfoProvider
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
